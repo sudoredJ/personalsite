e63d45a56052db25ddaa1ab370f4a5b7031895f2
Jared <j.mantell@wustl.edu>
2023-10-16 21:12:18 -0500

    a

--- src/components/gradientPointMaterial.js
+++ src/components/gradientPointMaterial.js
@@ -2,7 +2,7 @@ import { extend } from '@react-three/fiber';
 import { shaderMaterial } from '@react-three/drei';
 import glsl from 'glslify';
 
-const GradientPointMaterial = shaderMaterial(
+export const GradientPointMaterial = shaderMaterial(
   { color1: '#c850c0', color2: '#ffcc70' },
   glsl`
     varying vec3 vColor;
