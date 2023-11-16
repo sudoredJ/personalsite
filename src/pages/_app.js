029d066930b1ef585a88dd97a723e5290ee2f6c8
Jared <j.mantell@wustl.edu>
2023-11-16 15:29:24 -0600

    a

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,9 @@
 import Stars from '../components/Stars';
-import Cursor from '../components/Cursor';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Stars />
-      <Cursor />
       <Component {...pageProps} />
     </>
   );
