37a002097e31e7ca4c22cb1ea1dc2ef50a3cecc4
Jared <j.mantell@wustl.edu>
2023-11-19 21:47:57 -0500

    snasa

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,6 @@
-import Stars from '../components/Stars';
-import Cursor from '../components/Cursor';
-
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    <Cursor/>
       <Component {...pageProps} />
     </>
   );
