f37dc77ec5477797ec779655c413c24cd4324a30
Jared <j.mantell@wustl.edu>
2024-01-04 11:47:36 -0500

    ok

--- src/pages/project-page.js
+++ src/pages/project-page.js
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import Link from 'next/link';
 import { useEffect } from 'react';
 
@@ -20,11 +19,13 @@ const projectStyle = {
 export default function Projects() {
   return (
     <>
-      <Head>
-        <title>My Portfolio Projects</title>
-      </Head>
       <div style={containerStyle}>
-        <h1>Portfolio Projects</h1>
+        <h1>My Projects:</h1>
+        {/* Project 0: Centrifuge */}
+        <div style={projectStyle}>
+          <h2>Centrifuge</h2>
+          <a href="https://github.com/organizations/Centrifuge-Micro" target="_blank" rel="noopener noreferrer">View Project</a>
+        </div>
         {/* Project 1: Auto Sunset */}
         <div style={projectStyle}>
           <h2>Auto Sunset - Google Calendar Extension</h2>
@@ -32,15 +33,17 @@ export default function Projects() {
           <p><strong>Why:</strong> You only get one sunset a day. I grew up on Florida's Gulf coast, so I've been spoiled by sunsets gliding under Clearwater Beach's skyline while the sky and ocean turn gorgeous shades of purple. If you're working hard, you can miss the world passing by. There's only one sunset a day. Take the ten minutes and watch it, preferably someplace nice.</p>
           <a href="https://chromewebstore.google.com/detail/auto-sunset/nplhhaelnagjbbljmhijclndlnhecooj?hl=en" target="_blank" rel="noopener noreferrer">View Project</a>
         </div>
+        {/* Proj 3 VRClub*/}
+        <div style={projectStyle}>
+          <h2>VRClub</h2>
+          <p>Project ended Jan. 2023. Formerly second-largest Virtual-Reality forum on Discord. <br></br>Member of Discord Partner Program</p>
+                  </div>
         {/* Project 2: Rethinking Stereotypes */}
         <div style={projectStyle}>
-          <h2>Rethinking Stereotypes: A Demographic Analysis of Sexual Violence Perpetrators in Prisons</h2>
-          
-          <p><strong>Methodology:</strong> The study employed advanced data visualization tools in Tableau to interpret and present the data. This approach allowed for an in-depth examination of patterns and trends that might otherwise be overlooked in traditional analysis methods. The visualizations were designed to be intuitive yet detailed, providing clear insights into the demographic distribution of sexual violence perpetrators.</p>
-          
-          <p><strong>Findings and Value:</strong> The findings of this study were significant in highlighting the diversity among sexual violence perpetrators in prisons, contradicting the common perception of these individuals being predominantly older white males. This project not only contributes to academic discourse but also has practical implications for policy-making and correctional strategies. It underscores the need for a more nuanced understanding of sexual violence in correctional facilities.</p>
-                    
-          <a href="https://docs.google.com/presentation/d/10et8P5tWzDsn5RTp6BkPAmeC6Df6psMsOMvOIBIQ9EM/edit?usp=sharing" target="_blank" rel="noopener noreferrer">View Project</a>        </div>
+          <h2>Rethinking Stereotypes: Demographic Analysis of Sexual Violence Perpetrators in Prisons</h2>
+          <p>Project Conducted in Tableau</p>
+          <a href="https://docs.google.com/presentation/d/10et8P5tWzDsn5RTp6BkPAmeC6Df6psMsOMvOIBIQ9EM/edit?usp=sharing" target="_blank" rel="noopener noreferrer">View Project</a>
+        </div>
 
         <Link href="/">Back to Home</Link>
       </div>
