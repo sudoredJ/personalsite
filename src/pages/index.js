46b353b9e51c8fbda371e24d5eff9aeaca280c18
Jared M <j.mantell@wustl.edu>
2024-05-31 20:22:13 -0400

    accuracy

--- src/pages/index.js
+++ src/pages/index.js
@@ -76,7 +76,7 @@ export default function Home() {
   <div className="hero-wrapper" style={{ marginTop: '100px' }}>
   <Hero title="Hi, I'm Jared M." index className="bouncy">
           <p className="hero-description small width" style={{ textAlign: 'center' }}>
-        Thanks for stopping by my little corner of the net.
+        Thanks for stopping by my (unfinished & outdated) corner of the net.
       </p>
     </Hero>
   </div>
